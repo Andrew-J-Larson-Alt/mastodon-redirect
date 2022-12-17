5d1c11c9d98a2a1e425f6e72f3b45b3687655951
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2022-12-17 02:03:30 -0800

    Created new repo
    Heavily modified the mediafire repo to create this, in response to Twitter starting to block mastodon links.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# mastodon-redirect
- Since Twitter decided to block Mastodon URLs, I've created this redirect website to automatically redirect to your Mastodon profile URL.
+# masto-redir
+Since Twitter decided to block Mastodon URLs, I've created this redirect website to automatically redirect to your Mastodon profile URL.
+
+## Website Usage
+You can either go to the website, [Mastodon Redirector](https://thealiendrew.github.io/masto-redir/), directly and enter the Mastodon profile in the `username@mastodon.server` format. Or you can use the url in the following fashion to auto redirect:
+
+- Profile URL: `https://thealiendrew.github.io/masto-redir/?p=AlienDrew@mstdn.social`
+
+![Preview](https://github.com/TheAlienDrew/masto-redir/blob/main/img/readme/preview.png)
---
+++ update-mastodon-servers-list.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+URL="https://instances.social/list/old"
+JS_FILE="./js/custom.js"
+TEMP_FILE="temp.txt"
+
+# gets sorted array of mastodon servers
+servers=($(curl "${URL}" | grep -A 3 -F '<td class="table-success">UP</td>' | grep -oP 'scope="row".*href="https?://\K[^"]+' | sort))
+
+# creates JS object code
+jsObject="let mastodonServers = {"
+index=0
+echo -en "\nConverting server list to JS object"
+for i in "${servers[@]}"
+do
+  ! (( index % 500 )) && echo -n " ."
+  
+  jsObject="${jsObject}\"$i\": true"
+  index=$((index+1))
+  if [ $index -eq ${#servers[@]} ]; then
+    jsObject="${jsObject}};"
+  else
+    jsObject="${jsObject}, "
+  fi
+done
+echo "$jsObject" > "${TEMP_FILE}"
+sed -i -e '/let mastodonServers \=/{r '"${TEMP_FILE}"'' -e 'd}' "${JS_FILE}"
+rm -f "${TEMP_FILE}"
+echo -e "\nDone."
